5e22e984da3cf1109f6eee3e62fc0fb90644128d
harishreddy-g <harishreddy.g23@gmail.com>
2026-08-14 11:37:05 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.allow_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
